7eba566dcf0ffb02f33ac829e7b68019a1b3a3bc
chloe.spilker <chloe.spilker@CLC-LAB209-04.TCSD.tooelesd.org>
2018-10-09 14:10:49 -0600

    almost heaven

---
+++ src/cspik3/assignment_2/Assignment_2.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package cspik3.assignment_2;
+
+/**
+ * @author chloe.spilker
+ *
+ */
+public class Assignment_2 {
+
+	public static void main(String[] args) {
+		
+		// CODING SECTION
+		// #1
+		System.out.println("Welcome to Java\n");
+		System.out.println("Welcome to Computer Science\n");
+		System.out.println("Programming is Fun\n");
+		System.out.println("\n\n");
+		
+		// #2
+		System.out.println("Welcome to Java");
+		
+
+	}
+
+}
